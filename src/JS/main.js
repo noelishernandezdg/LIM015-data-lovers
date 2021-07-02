46a2888e1c74e0efdb8d23c5b44494b7cd7601bc
Grecia Llatas <greciastefanni96@gmail.com>
2021-07-02 17:10:36 -0500

    Se agregó lo realizado en Replit, como pantalla de bienvenida e inicio

---
+++ src/JS/main.js
@@ -0,0 +1,22 @@
+import { example } from './data.js';
+// import data from './data/lol/lol.js';
+import data from '../data/ghibli/ghibli.js';
+// import data from './data/rickandmorty/rickandmorty.js';
+
+console.log(example, data);
+
+document.getElementById("header").style.display = "none";
+
+const home = document.getElementById("btnHome");
+home.addEventListener("click" , homeFilms);
+
+function homeFilms() {
+    document.getElementById("btnHome").style.display="none";
+    document.getElementById("header").style.display= "block";
+}
+
+home.addEventListener("click", background);
+
+function background(){
+    const background = document.body.style.backgroundImage = "url(./img/fondoGaleria.jpg)";
+}
