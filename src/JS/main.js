4b773f323f0d6b1e5602c67dbb677cccb5cc9bbb
Grecia Llatas <greciastefanni96@gmail.com>
2021-07-05 19:29:00 -0500

    Mostrar los poster y titulos en Inicio

--- src/JS/main.js
+++ src/JS/main.js
@@ -21,3 +21,25 @@ function background() {
     document.body.style.backgroundImage = "url(./img/fondoGaleria.jpg)";
     document.body.style.backgroundSize = "cover";
 }
+
+//Crear la etiqueta img para los posters de las peliculas
+const figurePoster = document.querySelector("#figurePoster")
+
+const posters = data.films.map((item) => {
+    const createImg = document.createElement('img'); //Crear etiqueta img
+    createImg.src = `${item.poster}`; //Agregar la url del poster al src
+    figurePoster.appendChild(createImg); //convertimos la etiqueca en un nodo padre
+
+    const createTitle = document.createElement('h3');
+    const titleFilms = document.createTextNode(`${item.title}`);
+
+    createTitle.appendChild(titleFilms);
+    figurePoster.appendChild(createTitle);
+});
+
+// function x(item){
+//     console.table(item);
+// }
+
+// const y = data.films.filter(x);
+
