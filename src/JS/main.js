27ff72b4391aec42d8844ba1eb33b92bf4f97683
noelishernandezdg <noelishernandezdagraca@gmail.com>
2021-07-15 22:35:02 -0500

    mostrando info de peli, avance 4, incompleto

--- src/JS/main.js
+++ src/JS/main.js
@@ -6,7 +6,7 @@ const selectFilter = document.querySelector("#selectFilter");
 const galleryFilms = document.getElementById("fila");
 // const showInfo = document.getElementById("showInfo");
 const showInfoPoster = document.getElementById("showInfoPoster");
-const showInfoDescription = document.getElementById("showInfoDescription");
+// const showInfoDescription = document.getElementById("showInfoDescription");
 const figurePoster = document.querySelectorAll(".img-poster");
 // const ShowInfoPeople = document.getElementById("ShowInfoPeople");
 
@@ -65,9 +65,12 @@ function searchFilms(input, selector) {
 function showInfoFilms(data) {
     // galleryFilms.innerHTML = '';
     data.map((item) => {
-        showInfoPoster.innerHTML += "<figure> <img src=" + `${item.poster}` + " alt=''></figure>";
-        showInfoDescription.innerHTML += "<h1>" + `${item.title}` + "</h1><h3>" + `${item.release_date}` + "</h3><h2>" + `${item.description}` + "</h2><h3>" + "Director: " + `${item.director}` + "</h3><h3>" + "Producer: " + `${item.producer}` + "</h3>";
-    });
+            showInfoPoster.innerHTML += "<section><figure><img src=" + `${item.poster}` + " alt=''></figure><h1>" + `${item.title}` + "</h1><h3>" + `${item.release_date}` + "</h3><h2>" + `${item.description}` + "</h2><h3>" + "Director: " + `${item.director}` + "</h3><h3>" + "Producer: " + `${item.producer}` + "</h3></section>";
+            console.log(item.poster);
+        });
+    //     showInfoPoster.innerHTML += "<figure> <img src=" + `${item.poster}` + " alt=''></figure>";
+    //     showInfoDescription.innerHTML += "<h1>" + `${item.title}` + "</h1><h3>" + `${item.release_date}` + "</h3><h2>" + `${item.description}` + "</h2><h3>" + "Director: " + `${item.director}` + "</h3><h3>" + "Producer: " + `${item.producer}` + "</h3>";
+    // });
 }
 // console.log(showInfoFilms(data.films));
 // console.log(filterFilmsById(data.films));
